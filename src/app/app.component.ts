92033a20bf40f4f90cdaa16006876aac447ce0eb
Erick Rios <paulrios@outlook.es>
2023-11-01 12:27:55 -0500

    Probando lint staged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	message?: string;
 	title = 'angular-structure';
-	getMessages(): boolean {
-		return true;
+	getMessages(): string {
+		return 'Hola mundo';
 	}
 }
