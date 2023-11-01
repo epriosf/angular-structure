0dd5d3258857324b01c605ae8225b674ca11a6e4
Erick Rios <paulrios@outlook.es>
2023-11-01 10:55:31 -0500

    Probando primera forma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-structure';
+	message?: string;
+	title = 'angular-structure';
+	getMessages(): void {
+		console.log('error');
+	}
 }
