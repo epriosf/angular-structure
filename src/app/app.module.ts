6b9f368ed3c044024d09b5c0d31cfa6b0c3481d9
Erick Rios <paulrios@outlook.es>
2023-11-07 12:44:50 -0500

    fix: deleting unused dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { GridModule } from '@syncfusion/ej2-angular-grids';
-import { ListViewModule } from '@syncfusion/ej2-angular-lists';
-import { SidebarModule } from '@syncfusion/ej2-angular-navigations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 @NgModule({
-	declarations: [AppComponent, SidebarComponent],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		GridModule,
-		SidebarModule,
-		ListViewModule
-	],
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, GridModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
